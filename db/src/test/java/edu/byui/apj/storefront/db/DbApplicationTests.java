41b5e8d2fcd6897ca2186c62b84ef5ef822d6f61
derekgelveza <gel24002@byui.edu>
2026-03-21 11:53:39 -0600

    completed W11 Tutorial 11-3

--- db/src/test/java/edu/byui/apj/storefront/db/DbApplicationTests.java
+++ db/src/test/java/edu/byui/apj/storefront/db/DbApplicationTests.java
@@ -1,13 +1,17 @@
 package edu.byui.apj.storefront.db;
 
+import edu.byui.apj.storefront.db.messaging.OrderConfirmationProducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class DbApplicationTests {
 
+    @MockBean
+    OrderConfirmationProducer orderConfirmationProducer;
+
     @Test
     void contextLoads() {
     }
-
 }
